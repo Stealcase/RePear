b9d64db5eb5f3806da24e9e25b26e641d0fdb4e7
Benjamin Harbakk <stealcase@hotmail.com>
2020-01-31 23:53:51 +0100

    Made a scene, made Events, Spawned Pears

---
+++ RePear/Assets/PearCounterUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PearCounterUI : MonoBehaviour
+{
+        private TextMeshProUGUI textDisplay;
+        public IntVariable pearNumber;
+
+        private void Start()
+        {
+                textDisplay = GetComponentInChildren<TextMeshProUGUI>();
+        }
+
+        public void UpdatePearNumber()
+        {
+                textDisplay.text = pearNumber.Value.ToString();
+        }
+        
+}
---
+++ RePear/Assets/Scripts/PearSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityTemplateProjects
+{
+    public class PearSpawner : MonoBehaviour
+    {
+        public GameObject pearPrefab;
+        public IntVariable pearCounter;
+        private List<GameObject> pears;
+        public Transform spawnPosition;
+
+        public void Start()
+        {
+            pears = new List<GameObject>();
+            StartCoroutine("NumberedSpawner", 0.1f);
+        }
+
+        public IEnumerator NumberedSpawner()
+        {
+            int i = 2000;
+            int k = 0;
+            while (k < i)
+            {
+                SpawnPear();
+                yield return new WaitForSeconds(0.1f);
+                print("pear spawned");
+                k++;
+            }
+            
+        }
+        public void SpawnPear()
+        {
+            pears.Add(Instantiate(pearPrefab, spawnPosition));
+            pearCounter.ApplyChange(+1);
+        }
+
+        public void DisableGravity()
+        {
+            foreach (GameObject p in pears)
+            {
+                Rigidbody rb = p.GetComponent<Rigidbody>();
+                rb.useGravity = false;
+            }
+        }
+    }
+}
