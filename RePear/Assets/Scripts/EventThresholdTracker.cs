ce9f7cf1a5fce89856317b3387aa79ae9b46c34d
Benjamin Harbakk <stealcase@hotmail.com>
2020-02-01 18:51:27 +0100

    Made a VERY designer friendly Event System

---
+++ RePear/Assets/Scripts/EventThresholdTracker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+
+public class EventThresholdTracker : MonoBehaviour
+{
+    [FormerlySerializedAs("sacrifices")] public IntVariable intValue;
+    public IntVariable eventNumber;
+    public List<GameEvent> events;
+    public List<int> valueThresholds;
+
+
+    public void CheckThreshold()
+    {
+
+        if (intValue.Value > valueThresholds[eventNumber.Value] && (events.Count-1) >= eventNumber.Value)
+        {
+            if (events[eventNumber.Value] != null)
+            {
+                events[eventNumber.Value].Raise();
+                IncreaseEventNumber();
+
+            }
+            else
+            {
+                print("Sacrifices went out of range");
+            }
+        }
+    }
+    public void IncreaseEventNumber()
+    {
+        eventNumber.ApplyChange(+1);
+    }
+}
--- RePear/Assets/Scripts/PearCountReset.cs
+++ RePear/Assets/Scripts/PearCountReset.cs
@@ -4,11 +4,15 @@ namespace DefaultNamespace
 {
     public class PearCountReset : UnityEngine.MonoBehaviour
     {
-        public IntVariable pearCounter;
+        public IntVariable pearCounter, pearAdder, sacrifices, sacrificeEventNumber, totalEventNumber;
 
         public void Awake()
         {
             pearCounter.SetValue(0);
+            pearAdder.SetValue(0);
+            sacrifices.SetValue(0);
+            sacrificeEventNumber.SetValue(0);
+            totalEventNumber.SetValue(0);
         }
     }
 }
