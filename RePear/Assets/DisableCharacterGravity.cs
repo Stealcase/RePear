111b83689df1df709d65540adbcab37791402dd5
Benjamin Harbakk <stealcase@hotmail.com>
2020-02-02 16:28:07 +0100

    Committing

---
+++ RePear/Assets/DisableCharacterGravity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableCharacterGravity : MonoBehaviour
+{
+    public float force;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public IEnumerator sendFlying()
+    {
+        yield return new WaitForSeconds(2f);
+        rb.AddRelativeForce(0, force,0);
+    }
+
+    public void DisablePlayerGrav()
+    {
+        rb.useGravity = false;
+        rb.constraints = RigidbodyConstraints.None;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        StartCoroutine(sendFlying());
+    }
+}
